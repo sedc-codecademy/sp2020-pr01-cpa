4d64765c02a6a0db48e4976a64aaa3c0deddca5f
Manass2 <57198394+Manass2@users.noreply.github.com>
2020-05-07 22:04:43 +0200

    update

--- CarPoolApp/script.js
+++ CarPoolApp/script.js
@@ -28,7 +28,6 @@ baramPrevoz.addEventListener("click", () => {
         DOMElements.section2.style.display = "block";
         startCity.innerText = "Град на тргнување";
         endCity.innerText = "Град на пристигнување";
-        peopleData();
 });
 
 backButtonSection2.addEventListener("click", () => {
@@ -144,15 +143,7 @@ DOMElements.doneButton.addEventListener('click', () => {
 });
 
 
-// fetcovi
 
-async function peopleData() {
-    let response = await fetch(`https://github.com/sedc-codecademy/sp2020-pr01-cpa/blob/master/data.json`);
-    data = await response.json();
-    console.log(data);
-    // cosmonautInfo(data.results);
-    // addPagingButtons()
-}
 
 
 
